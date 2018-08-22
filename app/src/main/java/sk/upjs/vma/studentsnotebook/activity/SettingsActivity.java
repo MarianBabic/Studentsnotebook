90356920102bc2062f2644ecfce1801cc9fc2b5b
Marian Babic <babic_marian@solarturbines.com>
2018-08-22 22:46:59 +0200

    preferences activity created

--- app/src/main/java/sk/upjs/vma/studentsnotebook/activity/SettingsActivity.java
+++ app/src/main/java/sk/upjs/vma/studentsnotebook/activity/SettingsActivity.java
@@ -1,15 +1,64 @@
 package sk.upjs.vma.studentsnotebook.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Locale;
 
 import sk.upjs.vma.studentsnotebook.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preferences);
+
+        String languages_key = getResources().getString(R.string.languages_key);
+        this.findPreference(languages_key).setOnPreferenceChangeListener(
+                new OnPreferenceChangeListener() {
+
+                    @Override
+                    public boolean onPreferenceChange(Preference preference,
+                                                      Object newValue) {
+                        switchLanguage(newValue.toString());
+                        return true;
+                    }
+                });
     }
+
+    /**
+     * this method was inspired by this code:
+     * https://stackoverflow.com/questions/18582264/can-you-programmatically-switch-between-xml-resource-files-on-android
+     */
+    private void switchLanguage(String newLanguage) {
+        Resources res = getResources();
+        Configuration conf = res.getConfiguration();
+
+        String languages_en = getResources().getString(R.string.languages_en);
+        String languages_sk = getResources().getString(R.string.languages_sk);
+        Locale locale = newLanguage.equals(languages_sk) ? new Locale("sk") : new Locale("en");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            conf.setLocale(locale);
+        } else {
+            Locale.setDefault(locale);
+            conf.locale = locale;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            getApplicationContext().createConfigurationContext(conf);
+        } else {
+            res.updateConfiguration(conf, res.getDisplayMetrics());
+        }
+
+        Log.e("LANGUAGE", conf.locale.toString());
+    }
+
 }
